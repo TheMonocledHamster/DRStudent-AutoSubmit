79dd8331e05a3757c007048d5b982241226923ae
TheMonocledHamster <adithyah07@gmail.com>
2022-08-02 17:47:16 +0530

    python->python3

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import time
 
 while True:
     target_file = r"./Source/main.py"
-    subprocess.run("python "+target_file, shell=True)
+    subprocess.run("python3 "+target_file, shell=True)
     time.sleep(1800)
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
