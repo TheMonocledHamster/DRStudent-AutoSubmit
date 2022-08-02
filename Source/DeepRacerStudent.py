119150fd74c89e459b74525673df5a32cf249bbc
Adithya Hegde <adithyah07@gmail.com>
2022-08-02 22:15:50 +0530

    remove unnecessary imports

--- Source/DeepRacerStudent.py
+++ Source/DeepRacerStudent.py
@@ -1,5 +1,3 @@
-from multiprocessing.connection import wait
-from statistics import mode
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
