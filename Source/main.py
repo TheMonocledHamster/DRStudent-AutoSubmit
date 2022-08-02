4c0a3765aeac01314d6b11961480665182f9b6c7
Adithya Hegde <adithyah07@gmail.com>
2022-08-03 01:22:54 +0530

    bleh fix

--- Source/main.py
+++ Source/main.py
@@ -1,8 +1,12 @@
 from DeepRacerStudent import DeepRacerStudent
 import json
+import time
 
 if __name__ == "__main__":
     with open("./secrets/secrets.json") as config_file:
         config = json.load(config_file)
     control = DeepRacerStudent(config)
-    control.submit()
+    while True:    
+        control.submit()
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        time.sleep(1500)
